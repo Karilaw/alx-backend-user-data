b74d7c29f41167497fc67e8dcbe553fd331da1aa
Karilaw <lawrencekariithi6@gmail.com>
2024-02-20 08:17:54 +0000

    first commit to test

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ DB module
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -42,4 +43,28 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user by keyword arguments.
+
+        Parameters:
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            User: The first User object that matches the keyword arguments.
+
+        Raises:
+            NoResultFound: If no User object matches the keyword arguments.
+            InvalidRequestError: If wrong query arguments are passed.
+        """
+        if not kwargs:
+            raise InvalidRequestError("No keyword arguments provided.")
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound("No user found with the provided arguments.")
+
         return user
